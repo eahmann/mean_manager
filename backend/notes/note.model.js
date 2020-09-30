63b4bc0ee601cf97fee0c64e648382fd0a227e34
Camden Barta <camdenbarta@Camdens-MacBook-Pro.local>
2020-09-30 16:40:22 -0500

    Created Notes

---
+++ backend/notes/note.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const textSearch = require('mongoose-partial-full-search');
+const mongoose_fuzzy_searching = require('mongoose-fuzzy-searching');
+const { ObjectId } = require('mongodb');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    visibility: { type: Role, required: true},   //Creating classes for Notes
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    lastName: { type: String, required: true },
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        // remove these props when object is serialized
+        delete ret._id;
+    }
+});
+
+//schema.plugin(textSearch);
+
+schema.index({'$**': 'text'});
+
+module.exports = mongoose.model('Notes', schema);
