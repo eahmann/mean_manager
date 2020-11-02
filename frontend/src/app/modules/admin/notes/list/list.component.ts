7f1ff9a8a20e1bcaa4c5df6901bfde8106be8591
Camden Barta <camdenbarta@Camdens-MacBook-Pro.local>
2020-11-02 17:04:50 -0600

    Note Interface

--- frontend/src/app/modules/admin/notes/list/list.component.ts
+++ frontend/src/app/modules/admin/notes/list/list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Note } from '@core/models';
 import { NoteService } from '@core/services';
 import { first } from 'rxjs/operators';
 
@@ -8,7 +12,12 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  displayedColumns: string[] = ['created', 'account', 'title', 'description','visibility'];
+  dataSource: MatTableDataSource<Note>;
   notes: any[]
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor( 
   private noteService: NoteService
@@ -20,7 +29,18 @@ export class ListComponent implements OnInit {
       .subscribe(notes => {
         this.notes = notes;
         console.log(this.notes)
+        this.dataSource = new MatTableDataSource(this.notes);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
       })
+  }
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
--- frontend/src/app/modules/admin/notes/notes.module.ts
+++ frontend/src/app/modules/admin/notes/notes.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { NotesRoutingModule } from './notes-routing.module';
 import { AddEditComponent } from './add-edit/add-edit.component';
 import { ListComponent } from './list/list.component';
+import { MaterialModule } from '@shared/material';
 
 
 @NgModule({
   declarations: [AddEditComponent, ListComponent],
   imports: [
     CommonModule,
-    NotesRoutingModule
+    NotesRoutingModule,
+    MaterialModule
   ]
 })
 export class NotesModule { }
