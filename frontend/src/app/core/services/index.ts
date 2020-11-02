5077e9ada25bcaea185c4efa01e6da44ff92294b
Sean Knight <sceneknight@gmail.com>
2020-11-02 17:05:34 -0600

    Weirdness

--- frontend/src/app/core/services/index.ts
+++ frontend/src/app/core/services/index.ts
@@ -1,5 +1,6 @@
 ﻿export * from './account.service';
 export * from './alert.service';
+export * from './locations.service';
 export * from './project.service';
 export * from './navigate.service';
 //export * from './account.datasource';
