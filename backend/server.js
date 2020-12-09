b2fd4f9afa960e35bcccec5d2770e0c87e27aa1e
Sean Knight <sceneknight@gmail.com>
2020-12-09 15:20:53 -0600

    port 4000

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/api-docs', require('_helpers/swagger'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4300;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
